cdbb2f6ead839889c6cf2170e7231ba0a06a729f
Aminu Abubakar <aminuabubakar6361@gmail.com>
2026-09-19 16:02:02 +0100

    Add user routes to the server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorHandler');
 const healthRoutes = require('./routes/health.routes');
 const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(morgan('dev'));
 
 app.use('/api/health', healthRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
